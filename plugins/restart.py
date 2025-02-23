c2a9b8c0d579acf4c383265cb7aa3137ea70199c
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-23 13:38:43 +0600

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,8 +1,9 @@
 import sys
 from bot import Bot
+from config import OWNER_ID
 from pyrogram import Client, filters
 
-@Bot.on_message(filters.private & filters.command('restart') & filters.user(ADMINS))
+@Bot.on_message(filters.private & filters.command('restart') & filters.user(OWNER_ID))
 async def restart_bot(client: Bot, message: Message):
     await message.reply("Restarting the bot...")
     os.execv(sys.executable, ['python'] + sys.argv)
