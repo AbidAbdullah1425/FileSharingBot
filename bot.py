f1b94302d76ba32a2ccefea601b822e5f221e32b
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-24 00:20:38 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ class Bot(Client):
         self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")
 
   # Load settings before starting the bot
-        await load_settings()
+        load_settings()
 
         # Log FORCE_SUB_CHANNEL* variables
         self.LOGGER(__name__).info(f"FORCE_SUB_CHANNEL1: {FORCE_SUB_CHANNEL1}")
