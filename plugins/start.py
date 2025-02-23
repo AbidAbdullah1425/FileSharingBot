bb85fb5b55b2d41218dee54f51c737e01b9dc519
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-23 21:37:44 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -190,6 +190,29 @@ async def start_command(client: Client, message: Message):
             except Exception as e:
                 print(f"Error updating notification with 'Get File Again' button: {e}")
 
+    else:
+        reply_markup = InlineKeyboardMarkup(
+            [
+
+    [
+                    InlineKeyboardButton("• ᴀʙᴏᴜᴛ •", callback_data = "about")     
+
+    ]
+            ]
+        )
+        await message.reply_photo(
+            photo=START_PIC,
+            caption=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
+            ),
+            reply_markup=reply_markup#,
+            #message_effect_id=5104841245755180586  # 🔥
+        )
+
     await wait_msg.delete()
 
 #=====================================================================================##
