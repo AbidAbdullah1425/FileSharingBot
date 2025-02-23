3d37031388218ae9f81ac35c924f1d1b6e9cd052
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-23 23:17:07 +0600

    Update config.py

--- config.py
+++ config.py
@@ -75,7 +75,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b><blockquote>ᴊᴏɪɴ ʙᴇʟᴏᴡ ᴄʜᴀɴɴᴇʟs ᴛʜᴇɴ ᴘʀᴇss ʀᴇʟᴏᴀᴅ ʙᴜᴛᴛᴏɴ.ɪ ᴡɪʟʟ ʙᴇ ᴛʜᴇʀᴇ ᴡɪᴛʜ ʏᴏᴜʀ ᴇᴘɪsᴏᴅᴇ.</b></blockquote>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "ᴇɴᴄᴏᴅᴇᴅ ʙʏ <a href="https://t.me/HeavenlySubs">ʜᴇᴀᴠᴇɴʟʏ sᴜʙs</a>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "ᴇɴᴄᴏᴅᴇᴅ ʙʏ <a href='https://t.me/HeavenlySubs'>ʜᴇᴀᴠᴇɴʟʏ sᴜʙs</a>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
