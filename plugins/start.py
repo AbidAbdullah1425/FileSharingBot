ae11cc7fc495512f470d0603fb4e3a7a01a0c006
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-23 15:14:53 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -155,7 +155,7 @@ async def start_command(client: Client, message: Message):
                 pass
 
 
-if FILE_AUTO_DELETE > 0:
+    if FILE_AUTO_DELETE > 0:
     notification_msg = await message.reply(
         f"<b><blockquote>This file will be deleted in {get_exp_time(FILE_AUTO_DELETE)}. Please save or forward it to your saved messages before it gets deleted.</blockquote></b>"
     )
