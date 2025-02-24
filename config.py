f72805befa68a25b3dac489b5a4fafcd7d35c4d9
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-02-24 09:01:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,6 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 #Time in seconds for message delete, put 0 to never delete
 TIME = int(os.environ.get("TIME", "10"))
 
-from pymongo import MongoClient
-import os
-
 client = MongoClient(DB_URL)
 db = client[DB_NAME]
 collection = db['force_sub_channels']
