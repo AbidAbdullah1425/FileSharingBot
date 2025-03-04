b588afac7340ff2e9c8d2f3c13e8381cc0dc30a2
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-03-05 05:10:32 +0600

    Create total_users.py

---
+++ plugins/total_users.py
@@ -0,0 +1,7 @@
+WAIT_MSG = "<b>ᴡᴏʀᴋɪɴɢ....</b>"
+
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
+async def get_users(client: Bot, message: Message):
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    users = await full_userbase()
+    await msg.edit(f"{len(users)} users are using this bot")
